b60a040d765e0853665a27dd6d38ffe0964cc16e
bevkololi <kololibeverly@yahoo.com>
2018-04-24 15:54:05 +0300

    [tests] login and signup tests

---
+++ flask/test_auth.py
@@ -0,0 +1,64 @@
+from copy import deepcopy
+import unittest
+import json
+
+from app import app
+from app.models import users
+
+class AuthTestCase(unittest.TestCase):
+    """Test case for the authentication blueprint."""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.backup_users = deepcopy(app.users)
+        self.app = app.app.test_client()
+        self.app.testing = True
+        self.user_data = {
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+
+        
+
+    def test_registration(self):
+        """Test user registration works correcty."""
+        res = self.app.post('api/v1/auth/signup', data=self.user_data)
+        result = json.loads(res.data.decode())
+        self.assertEqual(
+            result['message'], "Sign up successful. Please login.")
+        self.assertEqual(res.status_code, 201)
+
+    def test_already_registered_user(self):
+        """Test that a user cannot be registered twice."""
+        res = self.app.post('api/v1/auth/signup', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        second_res = self.client().post('/auth/register', data=self.user_data)
+        self.assertEqual(second_res.status_code, 202)
+        result = json.loads(second_res.data.decode())
+        self.assertEqual(
+            result['message'], "User already exists. Please login.")
+
+    def test_user_login(self):
+        """Test registered user can login."""
+        res = self.app.post('api/v1/auth/login', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('api/v1/auth/login', data=self.user_data)
+        result = json.loads(login_res.data.decode())
+        self.assertEqual(result['message'], "You logged in successfully.")
+        self.assertEqual(login_res.status_code, 200)
+        
+
+    def test_non_registered_user_login(self):
+        """Test non registered users cannot login."""
+        not_a_user = {
+            'email': 'not_a_user@example.com',
+            'password': 'nope'
+        }
+        res = self.app.post('/auth/login', data=not_a_user)
+        result = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(
+            result['message'], "Invalid email or password, Please try again.")
+
+if __name__ == "__main__":
+    unittest.main()
