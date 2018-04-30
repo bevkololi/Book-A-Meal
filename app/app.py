c4ebda2780e41a423675932a2fed10b4264b11e0
bevkololi <kololibeverly@yahoo.com>
2018-04-30 17:03:38 +0300

    rewrite with pep8

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 """app.py is mainly used to run the tests"""
-#third party imports
+# third party imports
 from flask_api import FlaskAPI
 from flask import Flask, jsonify, abort, make_response, request
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,11 +7,11 @@ import uuid
 from functools import wraps
 import jwt
 import datetime
-#local imports
+# local imports
 from app import create_app
 from app.models import Meal, User
 
-list_users =[]
+list_users = []
 todays_menu = {}
 meals = []
 orders = []
@@ -21,28 +21,29 @@ app = Flask(__name__)
 NOT_FOUND = 'Not found'
 BAD_REQUEST = 'Bad request'
 
+
 def _get_meal(id):
-      return [meal for meal in meals if meal['id'] == id]
+    return [meal for meal in meals if meal['id'] == id]
 
 
 def _meal_exists(name):
-  return [meal for meal in meals if meal["name"] == name]
+    return [meal for meal in meals if meal["name"] == name]
+
 
 def _get_user(id):
-  return [user for user in list_users if user['id'] == id]
+    return [user for user in list_users if user['id'] == id]
 
 
 def _user_exists(email):
-  return [user for user in list_users if user["email"] == email]
+    return [user for user in list_users if user["email"] == email]
+
 
 def _get_order(order_id):
-  return [order for order in orders if order["order_id"] == order_id]
+    return [order for order in orders if order["order_id"] == order_id]
+
 
 def _order_exists(username):
-	return [order for order in orders if order["username"] == username]
-
-
-
+    return [order for order in orders if order["username"] == username]
 
 
 @app.route('/api/v1/meals', methods=['GET'])
@@ -71,10 +72,10 @@ def create_meal():
     if _meal_exists(name):
         abort(400)
     price = request.json.get('price')
-    if type(price) is not int:
+    if not isinstance(price, int):
         abort(400)
     meal = {"id": meal_id, "name": name,
-            "ingredients": ingredients,"price": price}
+            "ingredients": ingredients, "price": price}
     meals.append(meal)
     return jsonify({'meal': meal}), 201
 
@@ -90,7 +91,7 @@ def update_meal(id):
     name = request.json.get('name', meal[0]['name'])
     ingredients = request.json.get('ingredients', meal[0]['ingredients'])
     price = request.json.get('price', meal[0]['price'])
-    if type(price) is not int:
+    if not isinstance(price, int):
         abort(400)
     meal[0]['name'] = name
     meal[0]['ingredients'] = ingredients
@@ -105,10 +106,9 @@ def delete_meal(id):
     if meal:
         del meal
     else:
-        return jsonify ({'message': 'Meal does not exist'})
+        return jsonify({'message': 'Meal does not exist'})
 
 
-    
 @app.route('/api/v1/orders', methods=['GET'])
 def get_orders():
     """Get orders from orders list"""
@@ -135,10 +135,10 @@ def create_order():
     if _order_exists(username):
         abort(400)
     quantity = request.json.get('quantity')
-    if type(quantity) is not int:
+    if not isinstance(quantity, int):
         abort(400)
     order = {"order_id": order_id, "username": username,
-            "meal": meal,"quantity": quantity}
+             "meal": meal, "quantity": quantity}
     orders.append(order)
     return jsonify({'order': order}), 201
 
@@ -154,7 +154,7 @@ def update_order(order_id):
     username = request.json.get('username', order[0]['username'])
     meal = request.json.get('meal', order[0]['meal'])
     quantity = request.json.get('quantity', order[0]['quantity'])
-    if type(quantity) is not int:
+    if not isinstance(quantity, int):
         abort(400)
     order[0]['username'] = username
     order[0]['meal'] = meal
@@ -169,12 +169,13 @@ def delete_order(order_id):
     if order:
         del order
     else:
-        return jsonify ({'message': 'Order does not exist'})
+        return jsonify({'message': 'Order does not exist'})
 
 # #Get menu from menu list
 # @app.route('/api/v1/menu', methods=['GET'])
 # def get_menu():
 #     return jsonify({'menu': menu})
+
 
 @app.route('/api/v1/menu', methods=['POST'])
 def create_menu():
@@ -187,6 +188,7 @@ def create_menu():
 
     return jsonify({'menu': todays_menu}), 201
 
+
 @app.route('/api/v1/menu', methods=['GET'])
 def get_menu():
     return jsonify({'menu': todays_menu}), 201
@@ -198,16 +200,20 @@ def create_user():
     caterer = data.get('caterer', False)
 
     hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(user_id=4, username=data['username'], email=data['email'], password=hashed_password, caterer=caterer)
+    new_user = User(
+        user_id=4,
+        username=data['username'],
+        email=data['email'],
+        password=hashed_password,
+        caterer=caterer)
 
     list_users.append(new_user)
 
-    return jsonify({'message' : 'New user created!'}), 201
-
+    return jsonify({'message': 'New user created!'}), 201
 
 
 def get_by_email(email):
-    
+
     for user in list_users:
         if user.email == email:
             return user
@@ -221,9 +227,13 @@ def login():
 
     user = get_by_email(email)
     if user and check_password_hash(user.password, password):
-        token = jwt.encode({'user' : user.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=15)}, str(app.config['SECRET_KEY']))
-        return jsonify({'token' : token.decode('UTF-8'), 'message': 'Login successful!!'}), 200
-    return make_response(jsonify({'message':'Invalid email or password, Please try again.'}), 401)
+        token = jwt.encode({'user': user.username, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(seconds=15)}, str(app.config['SECRET_KEY']))
+        return jsonify({'token': token.decode('UTF-8'),
+                        'message': 'Login successful!!'}), 200
+    return make_response(
+        jsonify({'message': 'Invalid email or password, Please try again.'}), 401)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
