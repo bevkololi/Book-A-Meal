877292b5e99da942625e140c10f79c92534e1528
bevkololi <kololibeverly@yahoo.com>
2018-05-02 12:35:43 +0300

    indentation error

--- app/models.py
+++ app/models.py
@@ -43,38 +43,38 @@ class User(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    def generate_token(self, user_id):
-        """Generates the access token to be used as the Authorization header"""
+    # def generate_token(self, user_id):
+    #     """Generates the access token to be used as the Authorization header"""
 
-        try:
-            # set up a payload with an expiration time
-            payload = {
-                'exp': datetime.utcnow() + timedelta(minutes=5),
-                'iat': datetime.utcnow(),
-                'sub': user_id
-            }
-            # create the byte string token using the payload and the SECRET key
-            jwt_string = jwt.encode(
-                payload,
-                current_app.config.get('SECRET'),
-                algorithm='HS256'
-            )
-            return jwt_string
+    #     try:
+    #         # set up a payload with an expiration time
+    #         payload = {
+    #             'exp': datetime.utcnow() + timedelta(minutes=5),
+    #             'iat': datetime.utcnow(),
+    #             'sub': user_id
+    #         }
+    #         # create the byte string token using the payload and the SECRET key
+    #         jwt_string = jwt.encode(
+    #             payload,
+    #             current_app.config.get('SECRET'),
+    #             algorithm='HS256'
+    #         )
+    #         return jwt_string
 
-        except Exception as e:
-            # return an error in string format if an exception occurs
-            return str(e)
+    #     except Exception as e:
+    #         # return an error in string format if an exception occurs
+    #         return str(e)
 
-    @staticmethod
-    def decode_token(token):
-        """Decode the access token from the Authorization header."""
-        try:
-            payload = jwt.decode(token, current_app.config.get('SECRET'))
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return "Expired token. Please log in to get a new token"
-        except jwt.InvalidTokenError:
-            return "Invalid token. Please register or login"
+    # @staticmethod
+    # def decode_token(token):
+    #     """Decode the access token from the Authorization header."""
+    #     try:
+    #         payload = jwt.decode(token, current_app.config.get('SECRET'))
+    #         return payload['sub']
+    #     except jwt.ExpiredSignatureError:
+    #         return "Expired token. Please log in to get a new token"
+    #     except jwt.InvalidTokenError:
+    #         return "Invalid token. Please register or login"
 
 
 class Meal(db.Model):
