51353aab409fa5cb19eee30d159997f5c65c36bf
bevkololi <kololibeverly@yahoo.com>
2018-05-06 13:32:41 +0300

    [chore] refactoring order endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -51,40 +51,47 @@ def create_app(config_name):
                         if price:
                             int(price)
                         else:
-                            return {"message": "Price should be a number" 
-                     }, 200
+                            return {"message": "Price should be a number"}
                         if name:
                             meal = Meal(name=name, description=description, price=price)
                             meal.save()
                             response = jsonify({
+                                'message': 'Meal created successfully',
                                 'id': meal.id,
                                 'name': meal.name,
                                 'description': meal.description,
                                 'price': meal.price
                             })
-
-                            return make_response(response), 201
+                            response.status_code = 201
+                            return response
 
                         else:
                             return jsonify({'message': 'Please input the name of the meal'})
                     
                     else:
-                        return jsonify({'message': 'You are unauthorized to access this'})
+                        response = jsonify({"message": "You are unauthorized to access this"})
+                        response.status_code = 401
+                        return response
 
                 else:
-                    meals = Meal.get_all()
-                    results = []
+                    current_user = User.query.filter_by(id=user_id).first()
+                    if current_user.caterer:
+                        meals = Meal.get_all()
+                        results = []
 
-                    for meal in meals:
-                        obj = {
-                            'id': meal.id,
-                            'name': meal.name,
-                            'description': meal.description,
-                            'price': meal.price
-                        }
-                        results.append(obj)
+                        for meal in meals:
+                            obj = {
+                                'id': meal.id,
+                                'name': meal.name,
+                                'description': meal.description,
+                                'price': meal.price
+                            }
+                            results.append(obj)
 
-                    return make_response(jsonify(results)), 200
+                        return make_response(jsonify(results)), 200
+                    else:
+                        return jsonify({'message': 'You are not authorized to access this'})
+
             else:
                 # user is not legit, so the payload is an error message
                 message = user_id
@@ -173,7 +180,7 @@ def create_app(config_name):
 
     
     @app.route('/api/v1/myorders/', methods=['POST', 'GET'])
-    def orders():
+    def myorders():
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
 
@@ -234,38 +241,66 @@ def create_app(config_name):
             return make_response(jsonify(response)), 401
 
 
-    @app.route('/api/v1/orders/', methods=['GET'])
-    def adminorders():
+    @app.route('/api/v1/orders/', methods=['POST', 'GET'])
+    def orders():
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
+        
 
         if access_token:
             user_id = User.decode_token(access_token)
             if not isinstance(user_id, str):
-                current_user = User.query.filter_by(id=user_id).first()
-                if current_user.caterer:
-                    orders = Order.get_all(user_id=User.id)
-                    results = []
-                    
-                    for order in orders:
+                if request.method == "POST":
+                    current_user = User.query.filter_by(id=user_id).first()
+                    if current_user.caterer:
+                        meal = str(request.data.get('meal', ''))
+                        quantity = str(request.data.get('quantity', ''))
+                        ordered_by = Order.ordered_by
+                        if quantity:
+                                int(quantity)
+                        else:
+                            return {"message": "Quantity should be a number" 
+                     }, 200
+                        if meal:
+                            order = Order(meal=meal, quantity=quantity, ordered_by=user_id)
+                            order.save()
+                            response = jsonify({
+                                'id': order.id,
+                                'meal': order.meal,
+                                'time_ordered': order.time_ordered,
+                                'quantity': order.quantity,
+                                'ordered_by': user_id
+                            })
+
+                            return make_response(response), 201
+                        else:
+                            return jsonify({'message': 'Please input the meal name and quantity'})
+                        
+                    else:
+                        response = jsonify({"message": "You are unauthorized to access this"})
+                        response.status_code = 401
+                        return response
+
+                else:
+                    current_user = User.query.filter_by(id=user_id).first()
+                    if current_user.caterer:
+                        orders = Order.get_all(user_id)
+                        results = []
+
+                        for order in orders:
                             obj = {
                                 'id': order.id,
                                 'meal': order.meal,
                                 'time_ordered': order.time_ordered,
                                 'quantity': order.quantity,
                                 'ordered_by': order.ordered_by
-                         }
+                            }
                             results.append(obj)
-                            
 
-                    return make_response(jsonify(results)), 200
-                
-                else:
-                                  
-                    response = {
-                        'message': 'You are not authorized to perform these functions'
-                    }
-                    return make_response(jsonify(response)), 401
+                        return make_response(jsonify(results)), 200
+                    else:
+                        return jsonify({'message': 'You are not authorized to access this'})
+
             else:
                 # user is not legit, so the payload is an error message
                 message = user_id
@@ -273,6 +308,7 @@ def create_app(config_name):
                     'message': message
                 }
                 return make_response(jsonify(response)), 401
+
 
         else:
             response = {
@@ -482,7 +518,7 @@ def create_app(config_name):
                                 menu = Menu(date=date)
                                 menu.add_meal_to_menu(meals) 
                                 return {'message': 'Todays menu has been updated'}, 201
-                            return {'message': 'Please add menu items'}, 202
+                            return {'message': 'Please add meals to menu'}, 202
                         except Exception as error:
                             return {
                                 'message': 'an error occured',
