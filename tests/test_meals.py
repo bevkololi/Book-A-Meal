cb8de90a6dfa336c275b984c4e234c405aebb43a
bevkololi <kololibeverly@yahoo.com>
2018-05-10 16:26:30 +0300

    [tests] update on login test

--- tests/test_meals.py
+++ tests/test_meals.py
@@ -39,9 +39,9 @@ class MealTestCase(unittest.TestCase):
         }
         return self.client().post('/auth/login', data=user_data)
 
-    def login_admin(self, username= 'Some user', email="user@gmail.com", password="pass1234", caterer=True):
+    def login_admin(self, username= 'Some user', email="user@gmail.com", password="pass1234"):
         admin = User(username=username, email=email, password=password)
-        admin.caterer =caterer
+        admin.caterer = True
         admin.save()
         user_data = {
             'username': username,
