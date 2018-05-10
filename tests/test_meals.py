d9b61dee1e06c115d6996bbae943ec203dde267c
bevkololi <kololibeverly@yahoo.com>
2018-05-10 17:55:57 +0300

    [tests] update on login test

--- tests/test_meals.py
+++ tests/test_meals.py
@@ -61,7 +61,9 @@ class MealTestCase(unittest.TestCase):
 
     def test_admin_can_create_meals(self):
         """Test caterer can create a meal (POST request)"""
-        self.register_user()
+        result = self.register_user()
+        self.assertEqual(200, result.status_code)
+        result = self.login_admin()
         self.assertEqual(200, result.status_code)
         result = self.login_admin()
         self.assertEqual(200, result.status_code)
