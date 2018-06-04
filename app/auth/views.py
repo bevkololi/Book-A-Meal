3cfec9b89e43dd3b23d1a550bbc8b79d5607c873
bevkololi <kololibeverly@yahoo.com>
2018-06-04 11:28:58 +0300

    [chore] update for deployment

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,86 +1,69 @@
 from . import auth_blueprint
 
-from flask.views import MethodView
+from flask_restful import Resource, Api
 from flask import Blueprint, make_response, request, jsonify
 from app.models import User
 
 
-class RegistrationView(MethodView):
+class RegistrationView(Resource):
     """This class-based view registers a new user."""
 
     def post(self):
         # Query to see if the user already exists
-        user = User.query.filter_by(email=request.data['email']).first()
+        try:
+            post_data = request.get_json(force=True)
+            username = post_data['username']
+            email = post_data['email']
+            password = post_data['password']
 
-        if not user:
-            try:
-                post_data = request.data
+            user = User.query.filter_by(email=email).first()
+
+            if not user:
                 # Register the user
-                username = post_data['username']
-                email = post_data['email']
-                password = post_data['password']
                 user = User(email=email, password=password, username=username)
                 user.save()
 
-                response = {
-                    'message': 'You registered successfully. Please login.',
-                }
-                return make_response(jsonify(response)), 201
+                return {'message': 'You registered successfully. Please login.'}, 201
+            else:
+                return {'message': 'User already exists. Please login.'}, 202
+        except Exception as e:
+            return {'message': 'Registration not successful', 'error':str(e)}, 400
 
-            except Exception as e:
-                response = {
-                    'message': str(e)
-                }
-                return make_response(jsonify(response)), 401
-        else:
-            response = {
-                'message': 'User already exists. Please login.'
-            }
-
-            return make_response(jsonify(response)), 202
-
-
-class LoginView(MethodView):
+class LoginView(Resource):
     """This class-based view handles user login and access token generation."""
 
     def post(self):
         try:
-            user = User.query.filter_by(email=request.data['email']).first()
+            post_data = request.get_json(force=True)
+            username = post_data['username']
+            email = post_data['email']
+            password = post_data['password']
 
-            if user and user.password_is_valid(request.data['password']):
+            user = User.query.filter_by(email=email).first()
+
+            if user and user.password_is_valid(password):
                 # Generate the access token
                 access_token = user.generate_token(user.id)
                 if access_token:
-                    response = {
+                    return {
                         'message': 'You logged in successfully.',
                         'access_token': access_token.decode()
-                    }
-                    return make_response(jsonify(response)), 200
+                    }, 200
             else:
                 # User does not exist
-                response = {
+                return {
                     'message': 'Invalid email or password, Please try again.'
-                }
-                return make_response(jsonify(response)), 401
+                }, 401
 
         except Exception as e:
-            response = {
-                'message': str(e)
-            }
-            return make_response(jsonify(response)), 500
+            return {
+                'error': str(e),
+                'message': 'login unsuccessful'
+            }, 400
 
 
+AUTH = Blueprint('app.auth.views', __name__)
+API =Api(AUTH)
 # Define the API resource
-registration_view = RegistrationView.as_view('registration_view')
-login_view = LoginView.as_view('login_view')
-
-# Add the url rule for registering a user
-auth_blueprint.add_url_rule(
-    '/auth/signup',
-    view_func=registration_view,
-    methods=['POST'])
-auth_blueprint.add_url_rule(
-    '/auth/login',
-    view_func=login_view,
-    methods=['POST']
-)
+API.add_resource(RegistrationView, '/auth/signup', endpoint='signup')
+API.add_resource(LoginView, '/auth/login', endpoint='login')
